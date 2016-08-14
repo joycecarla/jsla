70f82274ad9851c6a1e9c5fe0a7ed7ae4e107e68
joycecarla <joycecarla016@yahoo.com.ph>
2016-08-14 12:31:59 +0800

    system?

---
+++ Software/Windows App/Juan Sumulong Learning Application/WindowsFormsApplication1/Data Struct/struct_student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace juan_sumulong_learning_app
+{
+    public struct Student
+    {
+        public string ID;
+        public string Lastname;
+        public string Firstname;
+        public string Middlename;
+        public int Birthday;
+        public string Birthmonth;
+        public int Birthyear;
+        public string AvatarPath;
+    }
+}
---
+++ Software/Windows App/Juan Sumulong Learning Application/WindowsFormsApplication1/Database.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace juan_sumulon_learning_app
+{
+    public static class Database
+    {
+
+    }
+}
